a80d8e282023c6df25497bad4067d913dd8ebaf2
Boris Kravchenko <allmycalifornia@gmail.com>
2024-10-25 12:50:38 +0300

    Initial Commit

---
+++ GiftWithTimerApp/GiftWithTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftWithTimerAppApp.swift
+//  GiftWithTimerApp
+//
+//  Created by Борис Кравченко on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GiftWithTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
